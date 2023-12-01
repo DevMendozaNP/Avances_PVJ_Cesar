b58dcbae126bb241b34db950ec0ac0e12028d32d
DevMendozaNP <cesmend.799@gmail.com>
2023-12-01 12:59:15 -0500

    Cinemachine
    Cámara

---
+++ ProyectoFinalPVJ-Avances-C-sar/ProyectoFinalPVJ-Avances-C-sar/Assets/Scripts/NewPlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class NewPlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float MovementSpeed = 3;
+
+    private Vector3 direction = Vector3.zero;
+    private CharacterController characterController;
+    private Animator animator;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (direction == Vector3.zero)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        else 
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        characterController.Move(
+            direction.normalized * Time.deltaTime * MovementSpeed
+        );
+
+    }
+
+    private void OnMove(InputValue value)
+    {
+        var data = value.Get<Vector2>();
+        direction = new Vector3(
+            data.x,
+            0f,
+            data.y
+        );
+    }
+
+    private void OnFire(InputValue value)
+    {
+
+    }
+}
